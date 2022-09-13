191534bb04b1de535a62977b8121511283dbe4e6
Valentina <vkpavlenina@mail.ru>
2022-09-13 23:49:56 +0500

    Task_43 project

---
+++ Task_43/Program.cs
@@ -0,0 +1,42 @@
+﻿double[,] coeff = new double[2, 2];
+double[] Point = new double[2];
+
+void InputCoefficients(){
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Result(double[,] coeff)
+{
+  Point[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  Point[1] = Point[0] * coeff[0,0] + coeff[0,1];
+  return Point;
+}
+
+void OutputResponse(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    Result(coeff);
+    Console.Write($"\nТочка пересечения прямых: ({Math.Round(Point[0],1)}; {Math.Round(Point[1],1)})");
+  }
+}
+
+InputCoefficients();
+OutputResponse(coeff);
